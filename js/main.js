03368b376e84ed8cd7e125e65d0300b77f043d8d
Kai-Arne Reiter <KReiter@SeaconLogistics.com>
2014-08-28 10:00:25 +0200

    changed home view to be generated

--- js/main.js
+++ js/main.js
@@ -12,14 +12,7 @@ var app = {
             }
         });
     },
-
-    initialize: function() {
-        var self = this;
-        this.store = new LocalStorageStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+    
     
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -27,7 +20,29 @@ var app = {
         } else {
             alert(title ? (title + ": " + message) : message);
         }
+    },
+    
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    
+    
+    initialize: function() {
+        var self = this;
+        this.store = new LocalStorageStore(function() {
+            self.renderHomeView();
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
 
 };
 
