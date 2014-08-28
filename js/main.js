c525472356ef5f3bcac13a7a95219eccc55d91d1
Kai-Arne Reiter <KReiter@SeaconLogistics.com>
2014-08-28 10:09:49 +0200

    added templates

--- js/main.js
+++ js/main.js
@@ -1,47 +1,36 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+	findByName: function() {
+		var self = this;
+		this.store.findByName($('.search-key').val(), function(employees) {
+			$('.employee-list').html(self.employeeLiTpl(employees));
+		});
+	},
     
     
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
-    },
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
     
-    renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+	renderHomeView: function() {
+		$('body').html(this.homeTpl());
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	},
     
     
     
-    initialize: function() {
-        var self = this;
-        this.store = new LocalStorageStore(function() {
-            self.renderHomeView();
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+	initialize: function() {
+		var self = this;
+		this.store = new LocalStorageStore(function() {
+			this.homeTpl = Handlebars.compile($("#home-tpl").html());
+			this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
     
 
 };
